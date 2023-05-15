b877d0921d272d23f2e5f4591210b41d128a4e96
Dyl6875 <132678439+Dyl6875@users.noreply.github.com>
2023-05-15 10:56:02 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+function Base () {
+    strip.showRainbow(1, 255)
+    strip.show()
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+strip.setBrightness(2)
+Base()
 basic.forever(function () {
-	
+    for (let index = 0; index <= 4; index++) {
+        strip.setBrightness(10 * index)
+        Base()
+        basic.pause(100)
+    }
 })
